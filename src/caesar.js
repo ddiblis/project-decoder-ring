c7c3bbc13c9b4ae97bcb56bf7495dd2cc35f5a63
Wafiq <thealiengunner@gmail.com>
2020-12-28 06:00:51 +0000

    Added required features pretaining to shifting in negative direction for the cesar shift, and a check on the substitution to see if an alphabet was given

--- src/caesar.js
+++ src/caesar.js
@@ -6,20 +6,21 @@ function caesar(word, shift, encode = true) {
   const encodedString = []
     
   if (encode === true) {
+    // checks if char is a letter, if it is, encodes it into ASCII modifies the number as appropriate and returns the correspnding letter
     word.split("").forEach(letter => {
-      // regex for if input is a letter
       if (letter.match(/[a-z]/i)) {
         encodedASCII = letter.toLowerCase().charCodeAt() + shift
-        // ASCII for the letter z is 122, so if it's above that, go ahead and 
-        if (encodedASCII > 122) {
-          // subtract 26(length of the english alphabet), this makes it so it loops to the beginning of the alphabet
-          encodedASCII = encodedASCII - 26
-          // decodes the ASCII to a string and pushes it to an array
+        console.log(encodedASCII)
+        if (encodedASCII < 97) {
+          encodedASCII = encodedASCII + 26
           encodedString.push(String.fromCharCode(encodedASCII))
-          // if it's not more than 122 just convert and push
+        }
+        else if (encodedASCII > 122) {
+          encodedASCII = encodedASCII - 26
+          encodedString.push(String.fromCharCode(encodedASCII))
         } else {encodedString.push(String.fromCharCode(encodedASCII))
           }
-          // if it's not a letter, go ahead and push it as is
+        // if it's not a letter, return as is
       } else {encodedString.push(letter)}
     })
 
@@ -27,17 +28,19 @@ function caesar(word, shift, encode = true) {
     word.split("").forEach(letter => {
   if (letter.match(/[a-z]/i)) {
     encodedASCII = letter.toLowerCase().charCodeAt() - shift
-    // same code as above but the letter a is 97 in ASCII
-    if (encodedASCII < 97) {
-      // add 26(length of alphabet) so it would loop from the end of the alphabet
+    if (encodedASCII > 122) {
+          encodedASCII = encodedASCII - 26
+          encodedString.push(String.fromCharCode(encodedASCII))
+        }
+    else if (encodedASCII < 97) {
       encodedASCII = encodedASCII + 26
       encodedString.push(String.fromCharCode(encodedASCII))
+      
     } else {encodedString.push(String.fromCharCode(encodedASCII))
       }
   } else {encodedString.push(letter)}
 })
   }
-  // join the items in the array which are the letters and return it
   return encodedString.join("")
 }
 
--- src/substitution.js
+++ src/substitution.js
@@ -1,30 +1,23 @@
 function substitution(string, alphabet, encode = true) {
-  // regex for if the string has any repeating letters included
-  if (alphabet.length !== 26 || /(.).*\1/.test(alphabet)) {
+  if (!alphabet || alphabet.length !== 26 || /(.).*\1/.test(alphabet)) {
     return false
   }
 
   const formattedString = []
-  // I felt lazy so I just wrote the letters in a row into a string and had JS split into an array for me
   const letters = "abcdefghijklmnopqrstuvwxyz".split("")
-  // turn the string input into an array
   const stringArr = string.split("")
-  // turn the alphabet into an array
   const alphabetArr = alphabet.split("")
 
   if (encode === true) {
     stringArr.forEach(letter => {
       for (pos in alphabetArr) {
         lowerLetter = letter.toLowerCase()
-        // if it's a space just return as is
         if (lowerLetter === " ") {
           formattedString.push(lowerLetter)
           // since there are 26 letters if there is no break it'll add 26 spaces
           break
         }
-        // this one is nifty, let me explain my logic here, since the length of the alphabet and the substitution alphabet is the same, and they need to completely and exactly overlap, if the letter position matches in the actual alphabet
         if (lowerLetter === letters[pos]) {
-          // just give me the same position in the array but from the substitution alphabet
           formattedString.push(alphabetArr[pos])
         }
       }
@@ -34,20 +27,16 @@ function substitution(string, alphabet, encode = true) {
     stringArr.forEach(letter => {
       for (pos in alphabetArr) {
         lowerLetter = letter.toLowerCase()
-        // same here for the spaces
         if (lowerLetter === " ") {
           formattedString.push(lowerLetter)
           break
         }
-        // and all I have to do here is reverse the previous solution, where if it matches the letter in the substitution alphabet, then the real letter will be in the exact same position in the proper alphabet
         if (lowerLetter === alphabetArr[pos]) {
-          // so return the alphabet item with the position where it matches the letter from the substitution alphabet
           formattedString.push(letters[pos])
         }
       }
     })
   }
-  // then just join the formatted sting and return it
   return formattedString.join("")
 }
 
